9f9b504dd1f9b84028d150850a39c4ee5d5d7bfb
Zander3312 <115901296+Zander3312@users.noreply.github.com>
2023-07-13 14:54:38 -0400

    Add files via upload

---
+++ Blackjack.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CSharpBlackjack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dealHand = new System.Windows.Forms.Button();
+            this.yourTotal = new System.Windows.Forms.Label();
+            this.hitButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.dealerTotal = new System.Windows.Forms.Label();
+            this.wins = new System.Windows.Forms.Label();
+            this.losses = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dealHand
+            // 
+            this.dealHand.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dealHand.Location = new System.Drawing.Point(81, 364);
+            this.dealHand.Name = "dealHand";
+            this.dealHand.Size = new System.Drawing.Size(167, 42);
+            this.dealHand.TabIndex = 0;
+            this.dealHand.Text = "Deal New Hand";
+            this.dealHand.UseVisualStyleBackColor = true;
+            this.dealHand.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yourTotal
+            // 
+            this.yourTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yourTotal.Location = new System.Drawing.Point(12, 307);
+            this.yourTotal.Name = "yourTotal";
+            this.yourTotal.Size = new System.Drawing.Size(776, 20);
+            this.yourTotal.TabIndex = 3;
+            this.yourTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hitButton
+            // 
+            this.hitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.hitButton.Location = new System.Drawing.Point(327, 364);
+            this.hitButton.Name = "hitButton";
+            this.hitButton.Size = new System.Drawing.Size(142, 42);
+            this.hitButton.TabIndex = 4;
+            this.hitButton.Text = "Hit";
+            this.hitButton.UseVisualStyleBackColor = true;
+            this.hitButton.Visible = false;
+            this.hitButton.Click += new System.EventHandler(this.hitButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.stopButton.Location = new System.Drawing.Point(570, 364);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(141, 42);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Visible = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // title
+            // 
+            this.title.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.title.Location = new System.Drawing.Point(12, 32);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(776, 36);
+            this.title.TabIndex = 2;
+            this.title.Text = "Welcome to Blackjack!";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dealerTotal
+            // 
+            this.dealerTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.dealerTotal.Location = new System.Drawing.Point(323, 307);
+            this.dealerTotal.Name = "dealerTotal";
+            this.dealerTotal.Size = new System.Drawing.Size(493, 20);
+            this.dealerTotal.TabIndex = 6;
+            this.dealerTotal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // wins
+            // 
+            this.wins.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.wins.Location = new System.Drawing.Point(688, 9);
+            this.wins.Name = "wins";
+            this.wins.Size = new System.Drawing.Size(100, 23);
+            this.wins.TabIndex = 7;
+            this.wins.Text = "Wins: 0";
+            this.wins.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // losses
+            // 
+            this.losses.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.losses.Location = new System.Drawing.Point(688, 32);
+            this.losses.Name = "losses";
+            this.losses.Size = new System.Drawing.Size(100, 23);
+            this.losses.TabIndex = 8;
+            this.losses.Text = "Losses: 0";
+            this.losses.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.losses);
+            this.Controls.Add(this.wins);
+            this.Controls.Add(this.dealerTotal);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.hitButton);
+            this.Controls.Add(this.yourTotal);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.dealHand);
+            this.Name = "Form1";
+            this.Text = "Blackjack";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dealHand;
+        private System.Windows.Forms.Label yourTotal;
+        private System.Windows.Forms.Button hitButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label dealerTotal;
+        private System.Windows.Forms.Label wins;
+        private System.Windows.Forms.Label losses;
+    }
+}
+
---
+++ Blackjack.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace CSharpBlackjack
+{
+    public partial class Form1 : Form
+    {
+        List<char> types = new List<char>()
+        {
+            '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A'
+        };
+        List<char> suits = new List<char>()
+        {
+            'C', 'S', 'H', 'D'
+        };
+        Random rnd = new Random();
+        List<Card> playerHand = new List<Card>();
+        List<Card> activeCards = new List<Card>();
+        List<PictureBox> cardImages = new List<PictureBox>();
+        List<PictureBox> dealerImages = new List<PictureBox>();
+        int yourScore = 0;
+        int winCount = 0;
+        int lossCount = 0;
+        bool aceInHand = false;
+        bool aceTaken = false;
+        int imageLocation = 240;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void addCardToHand(List<Card> hand, List<Card> activeCards)
+        {
+            bool dupe = false;
+            Card newCard;
+            while (true)
+            {
+                dupe = false;
+                newCard = new Card(types[rnd.Next(13)], suits[rnd.Next(4)]);
+                for (int i = 0; i < activeCards.Count(); i++)
+                {
+                    if (newCard.getType() == activeCards[i].getType() && newCard.getSuit() == activeCards[i].getSuit())
+                    {
+                        dupe = true;
+                        break;
+                    }
+                }
+                if (!dupe)
+                {
+                    break;
+                }
+            }
+            if (newCard.getType() == 'A')
+            {
+                aceInHand = true;
+            }
+            hand.Add(newCard);
+            activeCards.Add(newCard);
+        }
+
+        private void generateCardImage(Card card, List<PictureBox> imageList)
+        {
+            string cardName = Char.ToString(card.getType()) + Char.ToString(card.getSuit()) + ".png";
+            Console.WriteLine(card.getType());
+            Console.WriteLine(card.getSuit());
+            PictureBox newCard = new PictureBox
+            {
+                Name = cardName,
+                Location = new Point(imageLocation, 60),
+                Size = new Size(117,171),
+                BackgroundImageLayout = ImageLayout.Stretch,
+                BackgroundImage = Image.FromFile(
+                    Path.Combine(
+                        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                        "card-images\\" + cardName))
+            };
+            imageLocation += 25;
+            this.Controls.Add(newCard);
+            newCard.BringToFront();
+            imageList.Add(newCard);
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Deal New Hand
+        {
+            title.Text = "Your turn.";
+            hitButton.Visible = true;
+            stopButton.Visible = true;
+            dealerTotal.Visible = false;
+            yourTotal.Location = new Point(12, 267);
+            aceInHand = false;
+            aceTaken = false;
+            playerHand.Clear();
+            activeCards.Clear();
+            for (int i = 0; i < cardImages.Count(); i++)
+            {
+                Controls.Remove(cardImages[i]);
+                cardImages[i].Dispose();
+            }
+            cardImages.Clear();
+            for (int i = 0; i < dealerImages.Count(); i++)
+            {
+                Controls.Remove(dealerImages[i]);
+                dealerImages[i].Dispose();
+            }
+            dealerImages.Clear();
+            imageLocation = 240;
+            addCardToHand(playerHand, activeCards);
+            addCardToHand(playerHand, activeCards);
+            Console.WriteLine(playerHand[0].getType());
+            Console.WriteLine(playerHand[0].getSuit());
+            generateCardImage(playerHand[0], cardImages);
+            generateCardImage(playerHand[1], cardImages);
+            yourScore = playerHand[0].getValue() + playerHand[1].getValue();
+            if (aceInHand)
+            {
+                yourTotal.Text = "Your Score:" + yourScore + " or " + (yourScore - 10);
+            }
+            else
+            {
+                yourTotal.Text = "Your Score:" + yourScore;
+            }
+        }
+
+        private void hitButton_Click(object sender, EventArgs e)
+        {
+            addCardToHand(playerHand, activeCards);
+            generateCardImage(playerHand[playerHand.Count()-1], cardImages);
+            yourScore += playerHand[playerHand.Count()-1].getValue();
+            if (yourScore > 21 && aceInHand && !aceTaken)
+            {
+                yourScore = (yourScore - 10);
+                yourTotal.Text = "Your Score:" + yourScore;
+                aceTaken = true;
+            }
+            else if (aceInHand && !aceTaken)
+            {
+                yourTotal.Text = "Your Score:" + yourScore + " or " + (yourScore - 10);
+            }
+            else
+            {
+                yourTotal.Text = "Your Score:" + yourScore;
+            }
+            if (yourScore > 21)
+            {
+                hitButton.Visible = false;
+                stopButton.Visible = false;
+                title.Text = "Bust!";
+                lossCount += 1;
+                losses.Text = "Losses: " + lossCount;
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            imageLocation = 40;
+            yourTotal.Location = new Point(-180, 250);
+            dealerTotal.Visible = true;
+            for (int i = 0; i < cardImages.Count(); i++)
+            {
+                cardImages[i].Location = new Point(imageLocation, 60);
+                imageLocation += 25;
+            }
+            List<Card> dealerHand = new List<Card>();
+            aceInHand = false;
+            aceTaken = false;
+            addCardToHand(dealerHand, activeCards);
+            addCardToHand(dealerHand, activeCards);
+            int dealerScore = dealerHand[0].getValue() + dealerHand[1].getValue();
+            while (dealerScore <= 21 && dealerScore <= yourScore)
+            {
+                addCardToHand(dealerHand, activeCards);
+                dealerScore += dealerHand[dealerHand.Count() - 1].getValue();
+                if (dealerScore > 21 && aceInHand && !aceTaken)
+                {
+                    dealerScore = (dealerScore - 10);
+                    aceTaken = true;
+                }
+            }
+            imageLocation = 360;
+            for (int i = 0; i < dealerHand.Count(); i++)
+            {
+                generateCardImage(dealerHand[i], dealerImages);
+            }
+            if (dealerScore > 21)
+            {
+                dealerTotal.Text = "Dealer Score:" + dealerScore;
+                title.Text = "You win!";
+                winCount += 1;
+                wins.Text = "Wins: " + winCount;
+            }
+            else
+            {
+                dealerTotal.Text = "Dealer Score:" + dealerScore;
+                title.Text = "You lose!";
+                lossCount += 1;
+                losses.Text = "Losses: " + lossCount;
+            }
+            hitButton.Visible = false;
+            stopButton.Visible = false;
+        }
+    }
+}
+
+public class Card
+    {
+        private char type;
+        private char suit;
+        private int value;
+
+        public Card(char newType, char newSuit)
+        {
+            type = newType;
+            suit = newSuit;
+            if (newType == '0' || newType == 'J' || newType == 'Q' || newType == 'K') {
+                value = 10;
+            }
+            else if (newType == 'A')
+            {
+                value = 11;
+            }
+            else
+            {
+                value = (int)Char.GetNumericValue(newType);
+            }
+        }
+
+        public char getType()
+        {
+            return type;
+        }
+
+        public char getSuit()
+        {
+            return suit;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+    }
